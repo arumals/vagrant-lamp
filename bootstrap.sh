b0f50d47913d302abd78056be775ff7acb7bfb70
arumals <m4ltos@gmail.com>
2017-09-14 10:12:25 -0400

    Replaced say -> echo references

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,44 @@
-say "Update repositories"
+#!/bin/bash
+
+echo "Update ubuntu repositories ----------------"
 apt-get update
 
-say "Upgrade packages"
+echo "Upgrade installed packages ----------------"
 apt-get upgrade
 
-say "packages"
-apt-get install curl tree vim nano -y
+echo "Useful packages ---------------------------"
+apt-get install curl tree vim nano sed -y
 
-say "Install Apache"
+echo "Install Apache -----------------------------"
 apt-get install -y apache2
 
-say "Enable rewrites"
+echo "Enabling modrewrite ------------------------"
 a2enmod rewrite
 
-say "Install PHP"
+echo "Install PHP --------------------------------"
 apt-get -y install php apache2 libapache2-mod-php php-curl php-gd php-mysql php-gettext php-zip composer
 
-say "NodeJS"
+echo "Installing NodeJS --------------------------"
 apt-get -y install nodejs
 
-say "MySQL"
+echo "Update apache default vhost ----------------"
+
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    ServerName app.dev
+    DocumentRoot "/vagrant/public"
+    <Directory "/vagrant/public">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+service apache2 restart
+
+echo "Installing MySQL --------------------------"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
@@ -29,5 +48,5 @@ debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password root"
 debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
 apt-get -y install mysql-server phpmyadmin
 
-say "Restart Apache"
+echo "Restart Apache ----------------------------"
 service apache2 restart
